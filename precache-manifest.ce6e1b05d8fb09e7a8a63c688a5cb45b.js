e2c7bcc49e0f232903fa7c7b4231beaea9c97155
Kaija Dunklin <kaykay_s@yahoo.com>
2020-08-05 16:18:13 -0400

    Updates

---
+++ precache-manifest.ce6e1b05d8fb09e7a8a63c688a5cb45b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0daa232f64da7f5ab387c3f1173c32",
+    "url": "/poketimes/index.html"
+  },
+  {
+    "revision": "b5201f97d23be337dd7d",
+    "url": "/poketimes/static/css/main.56146529.chunk.css"
+  },
+  {
+    "revision": "517cbe35d36faa946d57",
+    "url": "/poketimes/static/js/2.a81e9432.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poketimes/static/js/2.a81e9432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5201f97d23be337dd7d",
+    "url": "/poketimes/static/js/main.68da0bb3.chunk.js"
+  },
+  {
+    "revision": "983edde110c01916bd2e",
+    "url": "/poketimes/static/js/runtime-main.e6cd60e1.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes/static/media/pokeball.f10bf078.png"
+  }
+]);
